f9dbb93cc16422c9c8c09e7e3d0b8ee3ab45ba29
saahi <saahimathi@gmail.com>
2021-11-13 08:47:23 +0530

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-import { getToken } from '../helpers/common'
 import styles from '../styles/Home.module.css'
 import axios from 'axios'
 
